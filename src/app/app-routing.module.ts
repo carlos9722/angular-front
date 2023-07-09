59957228004abc36dfc7ae5869b01d9d2f74f501
carlos9722 <carlos.9722@outlook.com>
2023-07-09 12:03:43 -0500

    Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PersonListComponent } from './components/person-list/person-list.component';
+import { PersonFormComponent } from './components/person-form/person-form.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: PersonListComponent
+  },
+  {
+    path: 'person',
+    component: PersonListComponent
+  },
+  {
+    path: 'person/create',
+    component: PersonFormComponent 
+  },
+  {
+    path: 'person/edit/:id',
+    component: PersonFormComponent
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// external
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PersonListComponent } from './components/person-list/person-list.component';
+import { PersonFormComponent } from './components/person-form/person-form.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PersonListComponent,
+    PersonFormComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
